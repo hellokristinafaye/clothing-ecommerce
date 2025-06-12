99e7c149f54cbfbf8056e03d44aa51f1ab468488
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 16:40:51 -0700

    collection element mounted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Collection from './pages/Collection';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/collection" element={<Collection/>} />
       </Routes>
 
     </div>
