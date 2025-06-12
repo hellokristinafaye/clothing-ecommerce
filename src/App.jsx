9a13f07b33ecd57334d1144be7aeab1bdc6dd17b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 16:42:32 -0700

    contact element mounted

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Collection from './pages/Collection';
 import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home/>} />
         <Route path="/collection" element={<Collection/>} />
         <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
 
     </div>
