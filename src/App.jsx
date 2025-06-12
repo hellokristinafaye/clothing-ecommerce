2de9c306a2e8649865ec8224021181c99abb603e
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 16:43:37 -0700

    product component mounted

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Collection from './pages/Collection';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Product from './pages/Product';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/collection" element={<Collection/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/contact" element={<Contact/>} />
+        <Route path="/product/:productId" element={<Product/>} />
       </Routes>
 
     </div>
