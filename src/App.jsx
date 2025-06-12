477c76ce892016103a972e61893c367a36f5342b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 16:39:57 -0700

    Home tag imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
 
       <Routes>
-
+        <Route path="/" element={<Home/>} />
       </Routes>
 
     </div>
