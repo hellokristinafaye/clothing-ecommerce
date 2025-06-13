8de20f57d5faae16ebdf062d547d7d30fb211c8e
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 21:05:01 -0700

    Code for a single line

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,8 @@ const Hero = () => {
           <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
               <div className="text-[#414141]">
                   <div className="flex items-center gap-2">
-                      
+                      <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+                      <p className=""></p>
                   </div>
             </div>
           </div>
