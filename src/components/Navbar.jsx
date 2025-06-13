c298c80d72f6f040fd500ebbe20d05e455db73b8
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 17:39:40 -0700

    underline displays/hides now based on link activity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,35 +4,26 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-          <img src={assets.logo} alt="" className="w-36" />
-          
-          <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
-              
-              <NavLink to='/' className="flex flex-col items-center gap-1">
-                  
-                  <p className="">HOME</p>
-                  <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
-                  
-              </NavLink>
-              <NavLink to='/collection' className="flex flex-col items-center gap-1">
-                  
-                  <p className="">COLLECTION</p>
-                  <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
-                  
-              </NavLink>
-              <NavLink to='/about' className="flex flex-col items-center gap-1">
-                  
-                  <p className="">ABOUT</p>
-                  <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
-                  
-              </NavLink>
-              <NavLink to='/contact' className="flex flex-col items-center gap-1">
-                  
-                  <p className="">CONTACT</p>
-                  <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
-                  
-              </NavLink>
-          </ul>
+      <img src={assets.logo} alt="" className="w-36" />
+
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p className="">HOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
+          <p className="">COLLECTION</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p className="">ABOUT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p className="">CONTACT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
     </div>
   );
 }
