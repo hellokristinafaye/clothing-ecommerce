9555891a62e0a7287221055061b1f150b3986a75
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 21:11:37 -0700

    Best Seller bar styled

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
               <div className="text-[#414141]">
                   <div className="flex items-center gap-2">
                       <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
-                      <p className="">OUR BEST SELLERS</p>
+                      <p className="font-medium text-sm md:text-base">OUR BEST SELLERS</p>
                   </div>
             </div>
           </div>
