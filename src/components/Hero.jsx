0496a27e319fee7796297f75e7b4289c5f838190
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 21:14:06 -0700

    style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,7 @@ const Hero = () => {
                       <p className="font-medium text-sm md:text-base">OUR BEST SELLERS</p>
                   </div>
                   <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">Latest Arrivals</h1>
+                  <div className="flex items-center gap-2"></div>
             </div>
           </div>
     </div>
