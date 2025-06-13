c3e74a4334d9a30f3bb2020b3876183eb643a5c9
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 22:28:13 -0700

    Title start

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({text1, text2}) => {
+  return (
+      <div className="inline-flex gap-2 items-center mb-3">
+          
+    </div>
+  )
+}
+
+export default Title
