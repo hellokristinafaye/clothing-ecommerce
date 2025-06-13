28042c74c9819c88679f5215fc6bfe1c8031a7d8
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 22:28:41 -0700

    Title text color

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Title = ({text1, text2}) => {
   return (
       <div className="inline-flex gap-2 items-center mb-3">
-          
+          <p className="text-gray-500"></p>
+          <p className=""></p>
     </div>
   )
 }
