af6a0cac7b77762cb435306c0523293d348c4cca
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 17:27:06 -0700

    navbar menu styling added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,11 @@ import { assets } from "../assets/frontend_assets/assets.js";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} alt="" className="w-36" />
+          <img src={assets.logo} alt="" className="w-36" />
+          
+          <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+              
+          </ul>
     </div>
   );
 }
