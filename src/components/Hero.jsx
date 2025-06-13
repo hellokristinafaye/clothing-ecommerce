a36a29d46ee221d76fa8345c77b1f0a2bf0afac0
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 21:12:03 -0700

    Latest Arrivals content added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,6 +10,7 @@ const Hero = () => {
                       <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
                       <p className="font-medium text-sm md:text-base">OUR BEST SELLERS</p>
                   </div>
+                  <h1 className="">Latest Arrivals</h1>
             </div>
           </div>
     </div>
