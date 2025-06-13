9cd9fdc423a696d0c3a7fd53fed464631d83c96e
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 21:02:21 -0700

    Style for left side of Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="flex flex-col sm:flex-row border border-gray-400 ">Hero</div>
+      <div className="flex flex-col sm:flex-row border border-gray-400 ">
+          {/* Hero Left Side */}
+          <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0"></div>
+    </div>
   )
 }
 
