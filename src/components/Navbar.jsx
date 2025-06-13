6d5d93ebee4c37a242636daaa62b42a057cc40f0
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 17:28:15 -0700

    Navlinks styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { assets } from "../assets/frontend_assets/assets.js";
-
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -8,6 +8,9 @@ const Navbar = () => {
           
           <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
               
+              <NavLink className="flex flex-col items-center gap-1">
+                  
+              </NavLink>
           </ul>
     </div>
   );
