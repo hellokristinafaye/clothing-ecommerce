40c52b328334588820f3aacbda2bdd7991fce004
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 21:03:05 -0700

    Color code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,11 @@ const Hero = () => {
   return (
       <div className="flex flex-col sm:flex-row border border-gray-400 ">
           {/* Hero Left Side */}
-          <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0"></div>
+          <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
+              <div className="text-[#414141]">
+                  
+            </div>
+          </div>
     </div>
   )
 }
