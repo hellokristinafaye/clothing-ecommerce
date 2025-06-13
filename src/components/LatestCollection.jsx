5e16ba29eb6f3c5d8aede64618e05feb5daedf58
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 22:37:06 -0700

    dummy text added

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -10,7 +10,8 @@ const LatestCollection = () => {
       <div className="my-10">
           <div className="text-center py-8 text-3xl">
               <Title text1={'LATEST'} text2={'COLLECTION'} />
-              <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600"></p>
+              <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos eum exercitationem.</p>
           </div>
       </div>
   )
