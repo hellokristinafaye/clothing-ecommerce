6898d5e881010f1211f441d143ee1a5ad92a9100
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 22:29:22 -0700

    Title content w/  variable

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = ({text1, text2}) => {
   return (
       <div className="inline-flex gap-2 items-center mb-3">
-          <p className="text-gray-500"></p>
+          <p className="text-gray-500">{text1} <span className="">{text2}</span></p>
           <p className=""></p>
     </div>
   )
