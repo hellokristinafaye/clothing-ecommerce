e1bc8e6cff5aea07e7af4edb30df560452dda695
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 22:08:31 -0700

    Shop Context function added

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,3 +1,14 @@
 import { createContext } from "react";
 
 export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+    
+    const value = {
+
+    }
+
+    return <ShopContext.Provider value={value}>
+        {props.children}
+    </ShopContext.Provider>;
+}
