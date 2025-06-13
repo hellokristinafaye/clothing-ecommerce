3c7dadd83d8ec452f9a9184f78a574da7adf0d20
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-12 22:36:16 -0700

    ptag styled

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -10,6 +10,7 @@ const LatestCollection = () => {
       <div className="my-10">
           <div className="text-center py-8 text-3xl">
               <Title text1={'LATEST'} text2={'COLLECTION'} />
+              <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600"></p>
           </div>
       </div>
   )
