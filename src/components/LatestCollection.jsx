f78b29c33059d174d52eec46cfe2b9f426c7d179
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-13 17:59:32 -0700

    State variable added

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
+    const [latestProducts, setLatestProducts] = useState([]);
 
   return (
       <div className="my-10">
@@ -13,6 +14,8 @@ const LatestCollection = () => {
               <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos eum exercitationem.</p>
           </div>
+
+
       </div>
   )
 }
