a9474c676cd5598b9c4ea7a1b56bca5426bc3d8d
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 14:31:48 -0700

    Placeholder text removed

--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NewsletterBox = () => {
   return (
-      <div className="text-center" > NewsletterBox
+      <div className="text-center" > 
           <p className="text-2xl font-medium text-gray-800">Subscribe now & get 20% off</p>
     </div >
   )
