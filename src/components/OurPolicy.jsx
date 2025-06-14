cbca87d9cd1598fa69435312dcf962d84b3f0f12
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 13:43:02 -0700

    Assets imported and image added

--- src/components/OurPolicy.jsx
+++ src/components/OurPolicy.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { assets } from '../assets/frontend_assets/assets'
 
 const OurPolicy = () => {
   return (
-    <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700">OurPolicy</div>
+      <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700">OurPolicy
+      
+          <div className="">
+              <img src={assets.exchange_icon} alt="" className="" />
+    </div>
+      </div>
   )
 }
 
