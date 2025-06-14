58c27cf389df83865939c48733f4f2c902da48fb
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 16:58:16 -0700

    Link text added/styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,11 @@ const Footer = () => {
               <div className="">
                   <img src={assets.logo} alt="" className="mb-5 w-32" />
                   <p className="w-full md:w-2/3 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi deserunt fugiat excepturi aperiam, sint ab, ex minus aliquid illum provident, officiis dolores voluptatibus repudiandae iusto! Neque accusamus ipsa eos molestiae.</p>
-            </div>
+              </div>
+              
+              <div className="">
+                  <p className="text-xl font-medium mb-5">COMPANY</p>
+              </div>
 
           </div>
     </div>
