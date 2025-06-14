0a56ea6f3960086c7854d9a67dd0f8fc7927eb09
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 14:32:14 -0700

    Text Styled

--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -4,6 +4,7 @@ const NewsletterBox = () => {
   return (
       <div className="text-center" > 
           <p className="text-2xl font-medium text-gray-800">Subscribe now & get 20% off</p>
+          <p className="text-gray-400 mt-3"></p>
     </div >
   )
 }
