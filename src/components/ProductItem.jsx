92c07498a97b46704f61aae8e9f71cd0632d6d40
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-13 18:11:41 -0700

    Price text styled

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -13,7 +13,7 @@ const ProductItem = ({ id, image, name, price }) => {
                   <img src={image[0]} alt="" className="hover:scale-110 transition ease-in-out" />
               </div>
               <p className="pt-3 pb-1 text-sm">{name}</p>
-              <p className="">{currency}{price}</p>
+              <p className="text-sm font-medium">{currency}{price}</p>
       </Link> 
 
       </div>
