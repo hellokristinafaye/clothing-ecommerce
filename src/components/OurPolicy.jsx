007fcedb844ed770f839c29e12f226ca53d29ae6
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 13:48:51 -0700

    Placeholder text removed

--- src/components/OurPolicy.jsx
+++ src/components/OurPolicy.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/frontend_assets/assets'
 
 const OurPolicy = () => {
   return (
-      <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700">OurPolicy
+      <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700">
       
           <div className="">
               <img src={assets.exchange_icon} alt="" className="w-12 m-auto mb-5" />
