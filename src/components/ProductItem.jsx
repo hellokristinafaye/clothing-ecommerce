b7e0f235f8c4f3d9ce7e77be472fe185cc59ca5c
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-13 18:10:40 -0700

    name text added/styled

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -10,8 +10,10 @@ const ProductItem = ({ id, image, name, price }) => {
     
           <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
               <div className="overflow-hidden">
-                  <img src={image[0]} alt="" className="" />
+                  <img src={image[0]} alt="" className="hover:scale-110 transition ease-in-out" />
               </div>
+              <p className="pt-3 pb-1 text-sm">{name}</p>
+              
       </Link> 
 
       </div>
