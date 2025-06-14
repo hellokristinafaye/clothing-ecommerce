20a2a4c8a8cd7ceab0bba83e70b60676dc1a9f1b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 13:44:17 -0700

    Title copy styled

--- src/components/OurPolicy.jsx
+++ src/components/OurPolicy.jsx
@@ -7,7 +7,7 @@ const OurPolicy = () => {
       
           <div className="">
               <img src={assets.exchange_icon} alt="" className="w-12 m-auto mb-5" />
-              <p className="">Easy Exchange Policy</p>
+              <p className="font-semibold">Easy Exchange Policy</p>
             </div>
       </div>
   )
