7aafea54c9854660e26a371107dd06e999416334
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 13:42:21 -0700

    text styling added

--- src/components/OurPolicy.jsx
+++ src/components/OurPolicy.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const OurPolicy = () => {
   return (
-    <div>OurPolicy</div>
+    <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700">OurPolicy</div>
   )
 }
 
