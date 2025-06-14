456d3ca4dd19c8e4357aebe926c165d5e636f067
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 13:33:40 -0700

    Adjusted number of Best Sellers in the slice

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -10,7 +10,7 @@ const BestSeller = () => {
 
     useEffect(() => {
         const bestProduct = products.filter((item) => (item.bestseller));
-        setBestSeller(bestProduct.slice(0,6));
+        setBestSeller(bestProduct.slice(0,5));
     }, [])
 
   return (
