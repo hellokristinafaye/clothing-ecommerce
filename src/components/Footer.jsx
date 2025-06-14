5f5f7a7f621b3a2115f52c958dd42fb4550524fb
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 16:54:58 -0700

    div styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+      <div>Footer
+          
+          <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_fr] gap-14 my-10 mt-40 text-sm">
+              
+          </div>
+    </div>
   )
 }
 
