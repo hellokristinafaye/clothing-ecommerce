df20ca692a6a6117112068210353b96fc4fce5b0
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 13:22:42 -0700

    states and useEffect started

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const BestSeller = () => {
+
+    const { products } = useContext(ShopContext);
+    const [bestSeller, setBestSeller] = useState([]);
+
+    useEffect(() => {
+        const bestProduct = products.filter((item) => (item.bestseller));
+        setBestSeller(bestProduct.slice(0,6));
+    }, [])
+
   return (
     <div>BestSeller</div>
   )
