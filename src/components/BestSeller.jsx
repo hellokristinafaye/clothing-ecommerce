a9c64a656114c37c76b5b0abb0ce3fb77224f091
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 13:30:52 -0700

    grid styling added

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -21,6 +21,9 @@ const BestSeller = () => {
               </p>
           </div>
 
+          {/* Rendering Products */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6"></div>
+
     </div>
   )
 }
