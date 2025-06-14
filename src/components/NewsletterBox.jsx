fdb0c7383a6a6b20eb433186550881343ec7620b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 14:33:39 -0700

    Form and Input field added

--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -5,7 +5,10 @@ const NewsletterBox = () => {
       <div className="text-center" > 
           <p className="text-2xl font-medium text-gray-800">Subscribe now & get 20% off</p>
           <p className="text-gray-400 mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-          
+
+          <form className="">
+              <input type="email" placeholder="Enter your email" className="" />
+          </form>
     </div >
   )
 }
