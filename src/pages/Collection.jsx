ee712d3dd54e67de00f8e338d53d27a8499bab3b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 18:10:05 -0700

    Text styling

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -16,7 +16,7 @@ const Collection = () => {
         <div className={`border border-gray-300 pl-5 mt-6 ${showFilter ? '':'hidden'} sm:block `}>
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-
+            <p className="flex gap-2"></p>
           </div>
         </div>
       </div>
