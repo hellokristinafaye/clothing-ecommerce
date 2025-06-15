daa820db997feca48b392ba9ee04177780e01cf1
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 17:01:18 -0700

    CTA UL styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,32 +3,36 @@ import { assets } from '../assets/frontend_assets/assets'
 
 const Footer = () => {
   return (
-      <div>Footer
-          
-          <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_fr] gap-14 my-10 mt-40 text-sm">
-              
-              <div className="">
-                  <img src={assets.logo} alt="" className="mb-5 w-32" />
-                  <p className="w-full md:w-2/3 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi deserunt fugiat excepturi aperiam, sint ab, ex minus aliquid illum provident, officiis dolores voluptatibus repudiandae iusto! Neque accusamus ipsa eos molestiae.</p>
-              </div>
-              
-              <div className="">
-                  <p className="text-xl font-medium mb-5">COMPANY</p>
-                  <ul className="flex flex-col gap-1 text-gray-600">
-                      <li className="">Home</li>
-                      <li className="">About Us</li>
-                      <li className="">Delivery</li>
-                      <li className="">Privacy Policy</li>
-                  </ul>
-              </div>
+    <div>
+      Footer
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_fr] gap-14 my-10 mt-40 text-sm">
+        <div className="">
+          <img src={assets.logo} alt="" className="mb-5 w-32" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+            deserunt fugiat excepturi aperiam, sint ab, ex minus aliquid illum
+            provident, officiis dolores voluptatibus repudiandae iusto! Neque
+            accusamus ipsa eos molestiae.
+          </p>
+        </div>
 
-              <div className="">
-                  <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
-              </div>
+        <div className="">
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li className="">Home</li>
+            <li className="">About Us</li>
+            <li className="">Delivery</li>
+            <li className="">Privacy Policy</li>
+          </ul>
+        </div>
 
-          </div>
+        <div className="">
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600"></ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
