6e78df2b5b028f262fd2ea05690756d7b203f26b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 18:14:32 -0700

    Title update

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -41,7 +41,7 @@ const Collection = () => {
             showFilter ? "" : "hidden"
           } sm:block `}
         >
-          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <p className="mb-3 text-sm font-medium">TYPE</p>
 
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
