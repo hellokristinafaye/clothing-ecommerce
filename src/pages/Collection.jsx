c55e8a35530989a9639f309b64c49aab405f76df
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 18:14:11 -0700

    SubCategories start

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -8,35 +8,58 @@ const Collection = () => {
  
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
-
       {/* Filter Options */}
       <div className="min-w-60">
-        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">FILTERS</p>
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          FILTERS
+        </p>
         {/* Category Filter */}
-        <div className={`border border-gray-300 pl-5 mt-6 ${showFilter ? '':'hidden'} sm:block `}>
+        <div
+          className={`border border-gray-300 pl-5 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block `}
+        >
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
 
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" value={'Men'} /> Men
+              <input type="checkbox" className="w-3" value={"Men"} /> Men
             </p>
 
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" value={'Women'} /> Women
+              <input type="checkbox" className="w-3" value={"Women"} /> Women
             </p>
 
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" value={'Kids'} /> Kids
+              <input type="checkbox" className="w-3" value={"Kids"} /> Kids
+            </p>
+          </div>
+        </div>
+        {/* SubCategory Filter */}
+        <div
+          className={`border border-gray-300 pl-5 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block `}
+        >
+          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Men"} /> Men
             </p>
 
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Women"} /> Women
+            </p>
+
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Kids"} /> Kids
+            </p>
           </div>
         </div>
       </div>
-
-
     </div>
-  )
+  );
 }
 
 export default Collection
