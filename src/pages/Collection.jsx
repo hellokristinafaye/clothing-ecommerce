1bcec9b21a6c4e11a873be5b2e779406185c83af
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 18:54:25 -0700

    All Products display!

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/frontend_assets/assets';
 import Title from '../components/Title';
+import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
@@ -17,9 +18,16 @@ const Collection = () => {
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
       {/* Filter Options */}
       <div className="min-w-60">
-        <p onClick={()=>setShowFilter(!showFilter)} className="my-2 text-xl flex items-center cursor-pointer gap-2">
+        <p
+          onClick={() => setShowFilter(!showFilter)}
+          className="my-2 text-xl flex items-center cursor-pointer gap-2"
+        >
           FILTERS
-          <img src={assets.dropdown_icon} alt="" className={`h-3 sm:hidden ${showFilter? 'rotate-90': ''}`} />
+          <img
+            src={assets.dropdown_icon}
+            alt=""
+            className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+          />
         </p>
         {/* Category Filter */}
         <div
@@ -70,28 +78,31 @@ const Collection = () => {
         </div>
       </div>
 
-    {/* Right Side */}
+      {/* Right Side */}
       <div className="flex-1">
         <div className="flex justify-between text-base sm:text-2xl mb-4">
-          <Title text1={'ALL'} text2={'COLLECTIONS'} />
-            {/* Product Sort */}
+          <Title text1={"ALL"} text2={"COLLECTIONS"} />
+          {/* Product Sort */}
           <select className="border-2 border-gray-300 text-sm px-2">
-            <option value="relevent" className="">Sort by: Relevant</option>
-            <option value="low-high" className="">Sort by: Low to High</option>
-            <option value="high-low" className="">Sort by: High to Low</option>
-            </select>
-
+            <option value="relevent" className="">
+              Sort by: Relevant
+            </option>
+            <option value="low-high" className="">
+              Sort by: Low to High
+            </option>
+            <option value="high-low" className="">
+              Sort by: High to Low
+            </option>
+          </select>
         </div>
-        
+
         {/* Map Products */}
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
-          
-          </div>
-
-        
-      </div>      
-      
-
+          {filterProducts.map((item, index) => (
+            <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
