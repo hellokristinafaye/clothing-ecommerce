0c6de4c2836393eea600c01d2abaa176f6f9a5cf
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 18:11:39 -0700

    First Option checkbox good

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -17,7 +17,7 @@ const Collection = () => {
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" />
+              <input type="checkbox" className="w-3" value={'Men'} /> Men
             </p>
           </div>
         </div>
