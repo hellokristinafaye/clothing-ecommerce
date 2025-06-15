f17f6e19db15455fe38452703669d7adaa5e618a
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 18:06:57 -0700

    OK the box isn't showing up unless there's content.. I bet we're adding stuff in a moment anyway so will keep placeholder for now

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
 
   const { products } = useContext(ShopContext);
-
+  const [showFilter, setShowFilter] = useState(false);
+ 
   return (
-    <div className="flex flex-col  sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
 
       {/* Filter Options */}
       <div className="min-w-60">
         <p className="my-2 text-xl flex items-center cursor-pointer gap-2">FILTERS</p>
+        {/* Category Filter */}
+        <div className={`border border-gray-300 pl-5 mt-6 ${showFilter ? '':'hidden'} sm:block `}>
+           placeholder
+        </div>
       </div>
 
 
