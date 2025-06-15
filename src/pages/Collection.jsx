a054e1edb6c686699e1e008e7767b65ac5b2bb27
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 18:12:21 -0700

    updated second option

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -15,10 +15,21 @@ const Collection = () => {
         {/* Category Filter */}
         <div className={`border border-gray-300 pl-5 mt-6 ${showFilter ? '':'hidden'} sm:block `}>
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+
             <p className="flex gap-2">
               <input type="checkbox" className="w-3" value={'Men'} /> Men
             </p>
+
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={'Women'} /> Women
+            </p>
+
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={'Men'} /> Men
+            </p>
+
           </div>
         </div>
       </div>
