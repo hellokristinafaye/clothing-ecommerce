0b3bcd31d4a56877142355004ae732e5b6de990a
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 17:51:23 -0700

    Styling start

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -6,7 +6,7 @@ const Collection = () => {
   const { products } = useContext(ShopContext);
 
   return (
-    <div>Collection</div>
+    <div className="flex flex-col  sm:flex-row gap-1 sm:gap-10 pt-10 border-t">Collection</div>
   )
 }
 
