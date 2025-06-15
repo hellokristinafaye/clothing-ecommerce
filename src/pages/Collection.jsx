b8a62fdceb3f8898b881aeb6742ba4d0a5c919f9
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-14 17:58:01 -0700

    Title styled

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -6,11 +6,11 @@ const Collection = () => {
   const { products } = useContext(ShopContext);
 
   return (
-    <div className="flex flex-col  sm:flex-row gap-1 sm:gap-10 pt-10 border-t">Collection
+    <div className="flex flex-col  sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
 
       {/* Filter Options */}
       <div className="min-w-60">
-        <p className="">FILTERS</p>
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">FILTERS</p>
       </div>
 
 
