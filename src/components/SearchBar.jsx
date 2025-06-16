4448b3932759108c70a0c978d47589f0c799ae2a
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 15:27:04 -0700

    Styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,7 +7,12 @@ const SearchBar = () => {
 
     
     return showSearch ? (
-        <div className="border-t border-b bg-gray-50 text-center">SearchBar</div>
+        <div className="border-t border-b bg-gray-50 text-center">SearchBar
+        
+            <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
+
+            </div>
+        </div>
     )
         : null
   ;
