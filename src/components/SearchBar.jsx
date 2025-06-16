90e016f74cc16f1c86e9863bf70763ff2ed373f3
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 15:22:53 -0700

    Searchbar mounted in App

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,10 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const SearchBar = () => {
+  const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
 
-    const {
-        search,
-        setSearch,
-        showSearch,
-        setShowSearch
-    } = useContext(ShopContext);
+  return <div>SearchBar</div>;
+};
 
-  return (
-    <div>SearchBar</div>
-  )
-}
-
-export default SearchBar
+export default SearchBar;
