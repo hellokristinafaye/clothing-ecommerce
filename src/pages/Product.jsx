5c01d9d03d19d637188f9b2ca0b9c0d7601e05f9
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 16:58:09 -0700

    Successfully getting product id

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom'
 
 const Product = () => {
 
-  const { productId} = useParams();
+  const { productId } = useParams();
+  console.log(productId)
 
   return (
     <div>Product</div>
