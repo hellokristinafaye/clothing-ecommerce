a795fa303194274fb0b0c354f1d376bbed64e6c7
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 15:40:04 -0700

    Logic to display SearchBar on Collections page start

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/frontend_assets/assets";
+import { useLocation } from "react-router-dom";
 
 const SearchBar = () => {
   const { search, setSearch, showSearch, setShowSearch } =
-    useContext(ShopContext);
+      useContext(ShopContext);
+    
+    const location = useLocation();
 
   return showSearch ? (
     <div className="border-t border-b bg-gray-50 text-center">
