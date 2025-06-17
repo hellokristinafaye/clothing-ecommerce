638fa81f921cc58e3db8d94029aeab5e629d09d7
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-17 13:55:10 -0700

    Logic started, div styling

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -31,6 +31,23 @@ const Cart = () => {
       <div className="text-2xl mb-3">
         <Title text1={'YOUR'} text2={'CART'} />
       </div>
+
+      <div className="">
+        {
+          cartData.map((item, index) => {
+            
+            const productData = products.find((product) => product._id === item._id);
+
+            return (
+              <div key={index} className="py-4 border-t border-b text-gray-700 grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
+
+              </div>
+            )
+
+          } )
+        }
+      </div>
+
     </div>
   )
 }
