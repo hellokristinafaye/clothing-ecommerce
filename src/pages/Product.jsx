5a71f95db25331ed1c795471c9d7925e9c511215
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 17:11:31 -0700

    Child styling

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -32,7 +32,9 @@ const Product = () => {
       <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
         {/* Product Images */}
         <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
-          <div className=""></div>
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+
+          </div>
 
         </div>
       </div>
