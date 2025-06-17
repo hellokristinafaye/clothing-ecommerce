16d6c833f2bf346f186e88ef9e3d5604c2adf3e8
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-17 14:40:48 -0700

    Re-styling container: using justify-between

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -39,23 +39,40 @@ const Cart = () => {
             const productData = products.find((product) => product._id === item._id);
 
             return (
-              <div key={index} className="py-4 border-t border-b text-gray-700 grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
-
-                <div className="flex items-start gap-6">
-                  <img src={productData.image[0]} alt="" className="w-16 sm:w-20" />
+              <div
+                key={index}
+                className="py-4 border-t border-b text-gray-700 grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+              >
+                <div className="flex items-start justify-between gap-6">
+                  <img
+                    src={productData.image[0]}
+                    alt=""
+                    className="w-16 sm:w-20"
+                  />
                   <div className="">
-                    <p className="text-xs sm:text-lg font-medium">{productData.name}</p>
+                    <p className="text-xs sm:text-lg font-medium">
+                      {productData.name}
+                    </p>
                     <div className="flex items-center gap-5 mt-2">
-                      <p className="">{currency}{productData.price}</p>
-                      <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">{item.size}</p>
+                      <p className="">
+                        {currency}
+                        {productData.price}
+                      </p>
+                      <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">
+                        {item.size}
+                      </p>
                     </div>
                   </div>
+
+                  <input
+                    type="number"
+                    min={1}
+                    defaultValue={item.quantity}
+                    className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1"
+                  />
                 </div>
-
-                  <input type="number" min={1} defaultValue={item.quantity} className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1" />
-
               </div>
-            )
+            );
 
           } )
         }
