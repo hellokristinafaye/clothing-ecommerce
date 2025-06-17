01dce7316189a7a360e0c47c2b19e253d72f3716
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 21:54:08 -0700

    useEffect logic start

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -1,10 +1,20 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 
 const RelatedProducts = ({ category, subCategory }) => {
     
     const { products } = useContext(ShopContext);
     const [related, setRelated] = useState([]);
+
+    useEffect(() => {
+       
+        if (products.length > 0) {
+            // creates a copy of all products
+            let productsCopy = products.slice();
+            productsCopy = productsCopy.filter((item)=>category === item.category)
+        }
+
+    },[products])
 
   return (
     <div>RelatedProducts</div>
