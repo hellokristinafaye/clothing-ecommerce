11f1059498f1984513efb28a8ccf67b0a2e990cf
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 17:41:57 -0700

    HR added

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -76,6 +76,7 @@ const Product = () => {
             </div>
           </div>
           <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer">ADD TO CART</button>
+          <hr className="mt-8 sm:w-4/5 g" />
         </div>
       </div>
     </div>
