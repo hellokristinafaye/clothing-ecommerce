1e75184f371e1e74e53b9a305dd320e2adc07208
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 17:20:06 -0700

    Other images can be selected now

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -36,7 +36,7 @@ const Product = () => {
           <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
             {
               productData.image.map((item, index) => (
-                <img src={item} key={index} alt="" className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" />
+                <img onClick={()=>setImage(item)} src={item} key={index} alt="" className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" />
               ))
               }
           </div>
