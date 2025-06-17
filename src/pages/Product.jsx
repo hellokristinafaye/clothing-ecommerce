fc83133b84c065b10bef7146f51e3ac12cf5f10a
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 17:26:16 -0700

    Dynamic Currency added

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,7 +7,7 @@ const Product = () => {
 
   const { productId } = useParams();
 
-  const { products } = useContext(ShopContext);
+  const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
 
@@ -28,18 +28,23 @@ const Product = () => {
   }, [productId, products])
 
 
-  return productData?  (
-    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">Product
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      Product
       {/* ------------ Product Data ----------- */}
       <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
         {/* ---------- Product Images -------------- */}
         <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
           <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
-            {
-              productData.image.map((item, index) => (
-                <img onClick={()=>setImage(item)} src={item} key={index} alt="" className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" />
-              ))
-              }
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                src={item}
+                key={index}
+                alt=""
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+              />
+            ))}
           </div>
           <div className="w-full sm:w-[80%]">
             <img src={image} alt="" className="w-full h-auto" />
@@ -48,9 +53,7 @@ const Product = () => {
         {/* ----------- Product Info ------------- */}
 
         <div className="flex-1">
-          <h1 className="font-medium text-2xl mt-2">
-            {productData.name}
-          </h1>
+          <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
 
           <div className="flex items-center gap-1 mt-2">
             <img src={assets.star_icon} alt="" className="w-3.5" />
@@ -58,13 +61,15 @@ const Product = () => {
             <img src={assets.star_icon} alt="" className="w-3.5" />
             <img src={assets.star_icon} alt="" className="w-3.5" />
             <img src={assets.star_dull_icon} alt="" className="w-3.5" />
-           <p className="pl-2">(122)</p>
+            <p className="pl-2">(122)</p>
           </div>
+          <p className="">{currency}</p>
         </div>
-        
       </div>
     </div>
-  ) : <div className="opacity-0"></div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 }
 
 export default Product
