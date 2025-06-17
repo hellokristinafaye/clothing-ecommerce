2cd6afd02819bbf06b129225f04faf8255e3d331
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 17:07:30 -0700

    Parent div styled w/ transition in

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -27,7 +27,10 @@ const Product = () => {
 
 
   return productData?  (
-    <div>Product</div>
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">Product
+
+
+    </div>
   ) : <div className="opacity-0"></div>
 }
 
