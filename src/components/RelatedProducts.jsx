c6f1292f08b01cc4b8e477284a6fa6b1be31d202
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 21:51:01 -0700

    Product data imported via products/ShopContext. State var start

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
-const RelatedProducts = () => {
+const RelatedProducts = ({ category, subCategory }) => {
+    
+    const { products } = useContext(ShopContext);
+    const [related, setRelated] = useState([]);
+
   return (
     <div>RelatedProducts</div>
   )
