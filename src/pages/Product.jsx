88ab3f6c4a955571dea60ff1519894a612b4b003
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 21:48:58 -0700

    Related products mounted

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/frontend_assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -108,7 +109,7 @@ const Product = () => {
 
       {/* ------------ Display Related Products --------------- */}
 
-      
+      <RelatedProducts/>
     </div>
   ) : (
     <div className="opacity-0"></div>
