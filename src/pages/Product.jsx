5e27b528696dc8aaa3654bc9d0f1ce219dec2496
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-16 21:44:28 -0700

    Description text styled

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,16 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { ShopContext } from '../context/ShopContext';
-import { assets } from '../assets/frontend_assets/assets';
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/frontend_assets/assets";
 
 const Product = () => {
-
   const { productId } = useParams();
 
   const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
-  const [image, setImage] = useState('');
-  const [size, setSize] = useState('');
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -20,13 +19,12 @@ const Product = () => {
         console.log(item);
         return null;
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     fetchProductData();
-  }, [productId, products])
-
+  }, [productId, products]);
 
   return productData ? (
     <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
@@ -63,39 +61,53 @@ const Product = () => {
             <img src={assets.star_dull_icon} alt="" className="w-3.5" />
             <p className="pl-2">(122)</p>
           </div>
-          <p className="mt-5 text-3xl font-medium">{currency} {productData.price}</p>
-          <p className="mt-5 text-gray-500 md:w-4/5 ">{productData.description}</p>
+          <p className="mt-5 text-3xl font-medium">
+            {currency} {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5 ">
+            {productData.description}
+          </p>
           <div className="flex flex-col gap-4 my-8">
             <p className="">Select Size</p>
             <div className="flex gap-2">
-              {
-                productData.sizes.map((item, index) => (
-                  <button onClick={()=>setSize(item)} key={index} className={`border py-2 px-4 bg-gray-100  ${item === size ? 'border-orange-500' : ''} cursor-pointer`}>{item}</button>
-                ))
-              }
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  key={index}
+                  className={`border py-2 px-4 bg-gray-100  ${
+                    item === size ? "border-orange-500" : ""
+                  } cursor-pointer`}
+                >
+                  {item}
+                </button>
+              ))}
             </div>
           </div>
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer">ADD TO CART</button>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer">
+            ADD TO CART
+          </button>
           <hr className="mt-8 sm:w-4/5 g" />
           <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1g">
-              <p className="">100% Original product.</p>
-              <p className="">Cash on Delivery is available on this product.</p>
-              <p className="">Easy return and exchange policy within 7 days.</p>
+            <p className="">100% Original product.</p>
+            <p className="">Cash on Delivery is available on this product.</p>
+            <p className="">Easy return and exchange policy within 7 days.</p>
           </div>
         </div>
       </div>
-
       {/* ------------ Description & Review Section --------------- */}
       <div className="mt-20">
         <div className="flex ">
           <b className="border px-5 py-3 text-sm">Description</b>
           <p className="border px-5 py-3 text-sm">Reviews (122)</p>
         </div>
-              </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+
+        </div>
+      </div>
     </div>
   ) : (
     <div className="opacity-0"></div>
   );
-}
+};
 
-export default Product
+export default Product;
