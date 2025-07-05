ea381c21bb861688187615db180e9db30a07b25d
Kristina Faye <hellokristinafaye@gmail.com>
2025-07-05 14:51:58 -0700

    Theme Switch button works! Now to refine

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Collection from './pages/Collection';
@@ -17,30 +17,40 @@ import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
 
+  const [theme, setTheme] = useState();
 
-  
+  const switchTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+    console.log(theme);
+  }
+
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <ToastContainer  />
-      <Navbar/>
+    <div
+      data-theme={theme}
+      className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"
+    >
+      <ToastContainer />
+      <Navbar />
+      <button onClick={switchTheme} className="theme-switch">
+        Theme Switch
+      </button>
       <SearchBar />
-      
+
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/collection" element={<Collection/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/product/:productId" element={<Product/>} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/place-order" element={<PlaceOrder/>} />
-        <Route path="/orders" element={<Orders/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
       </Routes>
 
-      <Footer/>
-
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App
